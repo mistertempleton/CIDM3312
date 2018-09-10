b2bb53074013d36c8bfac3124d5522c277e6b935
Alexander Roder <ar913509@wtstudents.wtamu.edu>
2018-09-10 12:01:28 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterestCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
